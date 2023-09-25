4e32dc5f74d55ae125f596f21193424beaefe876
anti-social-smiley <sylviokoopman54@gmail.com>
2023-09-25 16:25:39 -0700

    delete router

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -270,7 +270,7 @@ router.post('/login/:userId', (req, res) => {
 });
 
 // Route to delete the currently logged-in user
-router.post('/deleteUser', (req, res) => {
+router.post('/deleteUser/:UserId',checkPermissions(['delete']), (req, res) => {
   const token = req.header('Authorization');
 
   if (!token) {
