2644e705968152b7bcf007da8457cb5820fc5524
kinghiholike <etangoking6@gmail.com>
2023-09-25 14:55:17 +0100

    permissions added with acces roles.

---
+++ routes/permissions.js
@@ -0,0 +1,47 @@
+// PermissionsMiddleware.js
+
+
+//Roles with permissions
+const roles = {
+    user: {
+      can: ['read']
+    },
+    admin: {
+      can: ['read', 'write', 'delete','create']
+    }
+  };
+  
+//   const roles{
+//     user: {
+//         can: [],
+//     },
+//     admin: {
+//         can: []
+//     },
+//   }
+
+
+module.exports = function checkPermissions(permissions) {
+    return (req, res, next) => {
+      const { role } = req.user; 
+  
+      if (!role || !roles[role]) {
+        return res.status(403).json({ error: 'Access denied' });
+      }
+  
+      const allowedPermissions = roles[role].can;
+  
+      // Check if the user's role has the required permissions
+      const hasPermission = permissions.every((permission) =>
+        allowedPermissions.includes(permission)
+      );
+  
+      if (!hasPermission) {
+        return res.status(403).json({ error: 'Insufficient permissions' });
+      }
+  
+      // User has the required permissions, proceed to the route handler
+      next();
+    };
+  };
+  
