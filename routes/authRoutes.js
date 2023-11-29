ba32079f10eb8da458c5aa99cc23986139a4871f
kinghiholike <etangoking6@gmail.com>
2023-11-29 15:28:05 +0200

    updates

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -156,7 +156,7 @@ function authenticateToken(req, res, next) {
     return res.sendStatus(401);
   }
 
-  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+  jwt.verify(token, environment.SECRET_KEY, (err, user) => {
     if (err) {
       return res.sendStatus(403);
     }
