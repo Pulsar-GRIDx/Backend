2b1d6a2d9a24e5b738b2bc29509a4a17fc1e7523
kinghiholike <etangoking6@gmail.com>
2024-01-25 09:36:48 +0200

    Deployment Comit.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const corsOptions = {
 };
 // Rate limiter middleware
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
+  windowMs: 1 * 60 * 5,
   max: 5,
   message: 'Too many requests, please try again later.',
 });
