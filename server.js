4ce090b4404678dfa4c5fc58a02a32224158941b
kinghiholike <etangoking6@gmail.com>
2024-02-15 13:39:48 +0200

    bug fix.

---
+++ server.js
@@ -0,0 +1,15 @@
+import app from './app';
+
+
+const db = require("./config/db");
+
+db.connect((err) => {
+  if (err) {
+    console.log("Failed to connect to AWS RDS:", err.message);
+    return;
+  }
+  console.log("Successfully connected to AWS RDS database");
+  app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 4000}`);
+  });
+});
