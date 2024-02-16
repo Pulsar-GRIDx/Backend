dff6d39e0b3ff2493e1e25e7c24d9e9936bcdc51
kinghiholike <etangoking6@gmail.com>
2024-02-16 11:58:44 +0200

    bug

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -204,7 +204,7 @@ function authenticateToken(req, res, next) {
     return res.sendStatus(401);
   }
 
-  jwt.verify(token, environment.SECRET_KEY, (err, user) => {
+  jwt.verify(token, enviroment.SECRET_KEY, (err, user) => {
     if (err) {
       return res.sendStatus(403);
     }
@@ -213,6 +213,7 @@ function authenticateToken(req, res, next) {
     next();
   });
 }
+
 
 
 // Admin Route to update user information
